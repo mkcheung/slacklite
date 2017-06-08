8a7b6435744be1a9551116570397b0c0816f796a
Mars Cheung <marscheung@Marss-MacBook-Pro.local>
2017-06-07 20:31:29 -0700

    refit adjust socket command to utilize WAMP

--- src/AppBundle/Command/SocketCommand.php
+++ src/AppBundle/Command/SocketCommand.php
@@ -1,6 +1,7 @@
 <?php
 // myapplication/src/AppBundle/Command/SocketCommand.php
 // Change the namespace according to your bundle
+
 namespace AppBundle\Command;
 
 use Symfony\Component\Console\Command\Command;
@@ -8,10 +9,18 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+
+//include React and ZMQ modules
+use React\EventLoop\Factory;
+use React\ZMQ\Context;
+use React\Socket\Server as SocketServer;
+use ZMQ;
+
 // Include ratchet libs
-use Ratchet\Server\IoServer;
-use Ratchet\Http\HttpServer;
-use Ratchet\WebSocket\WsServer;
+use Ratchet\Server\IoServer as RatchetIoServer;
+use Ratchet\Http\HttpServer as RatchetHttpServer;
+use Ratchet\WebSocket\WsServer as RatchetWsServer;
+use Ratchet\Wamp\WampServer as RatchetWampServer;
 
 // Change the namespace according to your bundle
 use AppBundle\Sockets\Chat;
@@ -30,21 +39,37 @@ class SocketCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+
+        $loop   = Factory::create();
+        $pusher = new Chat();
+
+        // Listen for the web server to make a ZeroMQ push after an ajax request
+        $context = new Context($loop);
+        $pull = $context->getSocket(ZMQ::SOCKET_PULL);
+        $pull->bind('tcp://127.0.0.1:5555'); // Binding to 127.0.0.1 means the only client that can connect is itself
+        $pull->on('message', array($pusher, 'onChannelAccess'));
+
         $output->writeln([
             'Chat socket',// A line
             '============',// Another line
             'Starting chat, open your browser.',// Empty line
         ]);
-        
-        $server = IoServer::factory(
-            new HttpServer(
-                new WsServer(
-                    new Chat($this->getContainer())
+
+
+        // Set up our WebSocket server for clients wanting real-time updates
+        $webSock = new SocketServer($loop);
+        $webSock->listen(8090, '0.0.0.0'); // Binding to 0.0.0.0 means remotes can connect
+        $webServer = new RatchetIoServer(
+            new RatchetHttpServer(
+                new RatchetWsServer(
+                    new RatchetWampServer(
+                        $pusher
+                    )
                 )
             ),
-            8090
+            $webSock
         );
-        
-        $server->run();
+
+        $loop->run();
     }
 }
--- src/AppBundle/Sockets/Chat.php
+++ src/AppBundle/Sockets/Chat.php
@@ -4,84 +4,125 @@
 namespace AppBundle\Sockets;
 
 use Lexik\Bundle\JWTAuthenticationBundle\Security\Authentication\Token\JWTUserToken;
-use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
+use Ratchet\Wamp\WampServerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-class Chat implements MessageComponentInterface {
+class Chat implements WampServerInterface {
 
-    protected $clients;
-    protected $container;
+    protected $accessedChannels = [];
 
-
-    public function __construct(ContainerInterface $container) {
-        $this->container = $container;
-        $this->clients = new \SplObjectStorage;
+    public function onSubscribe(ConnectionInterface $conn, $topic) {
+        $this->accessedChannels[$topic->getId()] = $topic;
     }
-
+    public function onUnSubscribe(ConnectionInterface $conn, $topic) {
+    }
     public function onOpen(ConnectionInterface $conn) {
-        // Store the new connection to send messages to later
-        $this->clients->attach($conn);
-
         echo "New connection! ({$conn->resourceId})\n";
     }
-
-    public function onMessage(ConnectionInterface $from, $msg) {
-
-        $incomingPackage = json_decode($msg,true);
-        
-        $jwt_manager = $this->container->get('lexik_jwt_authentication.jwt_manager');
-        $userRepo = $this->container->get('repository.user');
-        $messageService = $this->container->get('app.message_service');
-
-        $token = new JWTUserToken();
-        $token->setRawToken($incomingPackage['authToken']);
-        $userInfo = $jwt_manager->decode($token);
-
-        $user = $userRepo->findOneBy(['user_id' => $userInfo['user_id']]);
-        if(!$user) {
-            $from->close();
-        }
-        $request = new Request(
-            $_GET,
-            $_POST,
-            [],
-            $_COOKIE,
-            $_FILES,
-            $_SERVER
-        );
-        $request->request->set('channel_id', $incomingPackage['channelId']);
-        $request->request->set('user_id', $user->getId());
-        $request->request->set('message', $incomingPackage['message']);
-
-        $message = $messageService->createMessage($request);
-
-        $numRecv = count($this->clients) - 1;
-        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
-            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
-
-        foreach ($this->clients as $client) {
-            // if ($from !== $client) {
-            if ($from === $client) {
-                $client->send($message);
-            }
-        }
-        return $message;
-    }
-
     public function onClose(ConnectionInterface $conn) {
-        // The connection is closed, remove it, as we can no longer send it messages
-        $this->clients->detach($conn);
-
-        echo "Connection {$conn->resourceId} has disconnected\n";
     }
-
-    public function onError(ConnectionInterface $conn, \Exception $e) {
-        echo "An error has occurred: {$e->getMessage()}\n";
-
+    public function onCall(ConnectionInterface $conn, $id, $topic, array $params) {
+        // In this application if clients send data it's because the user hacked around in console
+        $conn->callError($id, $topic, 'You are not allowed to make calls')->close();
+    }
+    public function onPublish(ConnectionInterface $conn, $topic, $event, array $exclude, array $eligible) {
+        // In this application if clients send data it's because the user hacked around in console
         $conn->close();
     }
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+    }
+
+    /**
+     * @param string JSON'ified string we'll receive from ZeroMQ
+     */
+    public function onChannelAccess($entry) {
+        echo 'IN ON CHANNEL ACCESS';
+        // $entryData = json_decode($entry, true);
+
+        // // If the lookup topic object isn't set there is no one to publish to
+        // if (!array_key_exists($entryData['category'], $this->accessedChannels)) {
+        //     return;
+        // }
+
+        // $topic = $this->accessedChannels[$entryData['category']];
+
+        // // re-send the data to all the clients subscribed to that category
+        // $topic->broadcast($entryData);
+    }
+
+    // protected $clients;
+    // protected $container;
+
+
+    // public function __construct(ContainerInterface $container) {
+    //     $this->container = $container;
+    //     $this->clients = new \SplObjectStorage;
+    // }
+
+    // public function onOpen(ConnectionInterface $conn) {
+    //     // Store the new connection to send messages to later
+    //     $this->clients->attach($conn);
+
+    //     echo "New connection! ({$conn->resourceId})\n";
+    // }
+
+    // public function onMessage(ConnectionInterface $from, $msg) {
+
+    //     $incomingPackage = json_decode($msg,true);
+        
+    //     $jwt_manager = $this->container->get('lexik_jwt_authentication.jwt_manager');
+    //     $userRepo = $this->container->get('repository.user');
+    //     $messageService = $this->container->get('app.message_service');
+
+    //     $token = new JWTUserToken();
+    //     $token->setRawToken($incomingPackage['authToken']);
+    //     $userInfo = $jwt_manager->decode($token);
+
+    //     $user = $userRepo->findOneBy(['user_id' => $userInfo['user_id']]);
+    //     if(!$user) {
+    //         $from->close();
+    //     }
+    //     $request = new Request(
+    //         $_GET,
+    //         $_POST,
+    //         [],
+    //         $_COOKIE,
+    //         $_FILES,
+    //         $_SERVER
+    //     );
+    //     $request->request->set('channel_id', $incomingPackage['channelId']);
+    //     $request->request->set('user_id', $user->getId());
+    //     $request->request->set('message', $incomingPackage['message']);
+
+    //     $message = $messageService->createMessage($request);
+
+    //     $numRecv = count($this->clients) - 1;
+    //     echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
+    //         , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
+
+    //     foreach ($this->clients as $client) {
+    //         // if ($from !== $client) {
+    //         // if ($from === $client) {
+    //             $client->send($message);
+    //         // }
+    //     }
+    //     return $message;
+    // }
+
+    // public function onClose(ConnectionInterface $conn) {
+    //     // The connection is closed, remove it, as we can no longer send it messages
+    //     $this->clients->detach($conn);
+
+    //     echo "Connection {$conn->resourceId} has disconnected\n";
+    // }
+
+    // public function onError(ConnectionInterface $conn, \Exception $e) {
+    //     echo "An error has occurred: {$e->getMessage()}\n";
+
+    //     $conn->close();
+    // }
 }
